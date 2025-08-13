f9fc22bed07f40b1a592e543a082d24073e22797
Cath <163967129+Catherinee18@users.noreply.github.com>
2025-08-13 08:38:15 +0800

    Update class_management.js

--- class_management.js
+++ class_management.js
@@ -33,26 +33,27 @@ const PORT = 3000;
 // })
 // });
 
-app.get(`/`, (req, res)=>{
-res.send(`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>
-        class management
-    </h1>
-</body>
-</html>
+// app.get(`/`, (req, res)=>{
+// res.send(`<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <h1>
+//         class management
+//     </h1>
+// </body>
+// </html>
     
-    `);
-});
+//     `);
+// });
 
-app.listen(PORT,()=>{
+// app.listen(PORT,()=>{
 
-console.log(`server is running on http://localhost:${PORT}`);
+// console.log(`server is running on http://localhost:${PORT}`);
 
-});
+
+// });
